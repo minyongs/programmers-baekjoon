52c98e9df3f68ecaff7b01b576452421c55d101d
Minhyung song <stylish_c@naver.com>
2023-12-31 13:49:55 +0900

    자연수 뒤집어 배열로 바꾸기

---
+++ src/programmers/Flip.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Flip {
+
+    public int[] solution(long n) {
+        String numToString = Long.toString(n); // n 을 문자열로 변환
+
+        int[] answer = new int [numToString.length()]; // 문자열의 길이 크기의 int 배열 생성
+
+        for(int i = 0, j = numToString.length()-1; i < numToString.length()&& j  >= 0 ; i++,j--) {
+            answer[i] = numToString.charAt(j) - '0'; // 교차해서 for문
+        }
+        return answer;
+
+
+
+    }
+
+}
+
+
+
+
