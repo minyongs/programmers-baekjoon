1581de58921d7ffd33c12b79d8aaa11aad3155e2
Minhyung song <stylish_c@naver.com>
2024-01-13 21:20:33 +0900

    줄 세우기

---
+++ src/BaekJoon_1681.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class BaekJoon_1681 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String N = sc.next();
+        int L = sc.nextInt();
+        int cnt = 0 ;
+
+        for(int j = 1 ; j <N.length(); j++)
+
+        //N까지 L이 포함되어 있으면
+        while(true){
+            for(int i = 1; i <Integer.parseInt(N);i ++){// 1부터 N까지
+
+            }
+
+
+            cnt ++ ;
+        }
+
+    }
+}
