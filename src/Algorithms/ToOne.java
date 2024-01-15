6689d37f3f2b6401ac1f2488d7739e3e1e306827
Minhyung song <stylish_c@naver.com>
2024-01-16 00:04:00 +0900

    그리디 예제

--- src/Algorithms/ToOne.java
+++ src/Algorithms/ToOne.java
@@ -10,15 +10,15 @@ public class ToOne {
 
 
         while (true) {
-        int target =(n/k) * k;
-        cnt +=( n-target);
+        int target =(n/k) * k; // 17/4 * 4 =16 -> 가장 가까운 4의 배수 만들기
+        cnt +=( n-target); // 17 -16 =1 -> 몇번 빼야할지 계산
         n= target;
 
         if(n<k) break;
 
-        cnt += 1;
+        cnt += 1; // cnt 1씩 증가시키면서 n을 k로 나누기
         n /= k ;
         }
-        cnt +=(n-1);
+        cnt +=(n-1); // 더이상 안나눠지니까 1될때까지 빼서 마무리
         System.out.println(cnt);
     }}
