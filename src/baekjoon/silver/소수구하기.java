e43e07954ae64257055d95cd62e53fcaedabaf2b
minyongs <stylish_c@naver.com>
2024-01-29 14:59:51 +0900

    소수 구하기

--- src/baekjoon/silver/소수구하기.java
+++ src/baekjoon/silver/소수구하기.java
@@ -6,21 +6,30 @@ public class 소수구하기 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        for(int i = a; i <= b; i++){
-            boolean isTrue = true;
-
-           //5
-            for(int j = 2;j < i;j++){
-                if(i % j == 0){// 5 를 2부터 4까지 나눠봤을때
-                    isTrue = false;
-                    break;
-                }
+        for(int i = a; i <=b; i++){
+            if(isPrime(i)){
+                System.out.println(i);
             }
+        }
 
         }
+
+        //소수를 판별하는 메서드 작성
+        public static boolean isPrime(int num){
+        if(num<2) return false; //1은 소수 아니다
+            //2부터 j의 제곱이 num 이하일때까지 -> 소수는 제곱근까지만 확인하면 된다!!
+        for(int j =2; j*j<=num;j++){ //나누어 떨어지는 수가 있으면 소수 아님
+            if(num%j==0){
+                return false;
+            }
+        }
+        return true; // 없으면 소수
+        }
     }
-}
+
+
+
+
