6fb146d5a6eaa799f89c2d1775bf77d8d338e966
Minhyung song <stylish_c@naver.com>
2024-02-14 23:41:03 +0900

    로프

---
+++ src/baekjoon/silver/로프.java
@@ -0,0 +1,32 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+public class 로프 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        for(int i = 0 ; i< n; i ++){
+            arr[i] = Integer.parseInt(br.readLine());
+
+        }
+
+        Arrays.sort(arr);
+        int[] arr2 = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            arr2[i] = arr[i] *(n - i );
+        }
+
+        Arrays.sort(arr2);
+        System.out.println(arr2[arr2.length-1]);
+
+    }
+}
