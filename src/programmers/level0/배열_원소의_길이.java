4da41b8a2351ad2b5750bc74e8c0261459d15475
Minhyung song <stylish_c@naver.com>
2024-03-04 21:57:37 +0900

    배열 원소의 길이

---
+++ src/programmers/level0/배열_원소의_길이.java
@@ -0,0 +1,12 @@
+package programmers.level0;
+
+public class 배열_원소의_길이 {
+
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        for(int i = 0; i <strlist.length; i++){
+            answer[i] = strlist[i].length();
+        }
+        return answer;
+    }
+}
