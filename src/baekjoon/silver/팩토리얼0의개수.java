18a447ad9a51622d4049cb2eab9871d6b88c5765
Minhyung song <stylish_c@naver.com>
2024-03-06 20:40:51 +0900

    팩토리얼 0의 개수

---
+++ src/baekjoon/silver/팩토리얼0의개수.java
@@ -0,0 +1,15 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 팩토리얼0의개수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+
+    }
+}
