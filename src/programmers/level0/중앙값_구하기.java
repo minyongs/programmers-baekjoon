0d46e527282f093a11effc6e238fbcab41e3b049
minyongs <stylish_c@naver.com>
2024-03-11 10:21:50 +0900

    중앙값 구하기

---
+++ src/programmers/level0/중앙값_구하기.java
@@ -0,0 +1,13 @@
+package programmers.level0;
+
+import java.util.Arrays;
+
+public class 중앙값_구하기{
+
+        public int solution(int[] array) {
+            int answer = 0;
+            Arrays.sort(array);
+            answer = array[array.length / 2];
+            return answer;
+        }
+}
