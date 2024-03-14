a58a984cef4d883a300da097d98cc77ed29f2260
Minhyung song <stylish_c@naver.com>
2024-03-14 16:09:05 +0900

    나이순 정렬

---
+++ src/baekjoon/silver/나이순정렬.java
@@ -0,0 +1,17 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 나이순정렬 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String[][] s = new String[n][n];
+        for(int i = 0 ; i < n ; i ++){
+
+        }
+    }
+}
