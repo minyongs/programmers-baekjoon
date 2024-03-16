17066704677cd3fb64b0523a57b44f9cd2048bd4
Minhyung song <stylish_c@naver.com>
2024-03-16 17:39:43 +0900

    123 더하기

---
+++ src/baekjoon/silver/일이삼더하기.java
@@ -0,0 +1,15 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 일이삼더하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+
+    }
+}
