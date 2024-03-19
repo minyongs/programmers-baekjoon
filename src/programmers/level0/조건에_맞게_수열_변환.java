d40830998bda820016b588bfcc67e9331ede9930
Minhyung song <stylish_c@naver.com>
2024-03-19 10:14:41 +0900

    조건에 맞게 수열 변환

---
+++ src/programmers/level0/조건에_맞게_수열_변환.java
@@ -0,0 +1,21 @@
+package programmers.level0;
+
+public class 조건에_맞게_수열_변환 {
+
+    public int[] solution(int[] arr) {
+        int[] answer = new int[arr.length];
+
+        for(int i = 0 ; i < arr.length;i++){
+            if(arr[i]>=50 && arr[i]%2 ==0){
+                answer[i] = arr[i]/2;
+            }else if(arr[i]<50 && arr[i]%2==1){
+                answer[i] = arr[i] * 2;
+            }else
+
+                answer[i] = arr[i];
+        }
+
+        return answer;
+    }
+
+}
